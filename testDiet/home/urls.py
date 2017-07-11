2048beefc24ebb99b9219fa65469ec1112a15dce
Nam Do <coffeeforthoughts@gmail.com>
2017-07-11 19:50:53 +1000

    added templates

--- testDiet/home/urls.py
+++ testDiet/home/urls.py
@@ -4,7 +4,10 @@ from . import views # Importing current app so we can refference the current vie
 urlpatterns = [
 
     url(r'home', views.index, name = 'home'),
-    url(r'carb', views.carb, name = 'carb')
+    url(r'carb', views.carb, name = 'carb'),
+    url(r'fruit', views.fruit, name = 'fruit'),
+    url(r'protein', views.protein, name = 'protein'),
+    url(r'veg', views.vegetable, name = 'vegetable'),
     # url(r'^store/', 'views.store', name = 'store'),
 
 ]
