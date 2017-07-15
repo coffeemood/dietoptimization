a3d324f704582199a0fbc50266c7c90c6b43bd7b
Nam Do <coffeeforthoughts@gmail.com>
2017-07-15 22:51:34 +1000

    missed smt

--- testDiet/home/views.py
+++ testDiet/home/views.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from collections import OrderedDict
 from home.models import Data, Food
-import os, csv
+import os, csv, subprocess
 
 # Create your views here.
 
