61a648b062d471cfe457eb75dc7a87ea585dfb7c
Nam Do <coffeeforthoughts@gmail.com>
2017-07-15 22:09:57 +1000

    wsgi

--- testDiet/testDiet/wsgi.py
+++ testDiet/testDiet/wsgi.py
@@ -7,10 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-import os
+import os,sys
 
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append("/home/ubuntu/dietoptimization/testDiet")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testDiet.settings")
 
 application = get_wsgi_application()
